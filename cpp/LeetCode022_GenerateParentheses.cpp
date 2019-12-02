e84bd517ecca50b269e6362470ac01248722b0e7
MageChiu <charles.r.chiu@gmail.com>
2019-12-03 02:12:24 +0800

    add 22 code

--- cpp/LeetCode022_GenerateParentheses.cpp
+++ cpp/LeetCode022_GenerateParentheses.cpp
@@ -9,33 +9,26 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         string test = string("");
-        generateParenthesisString(test, n, n);
         vector<string> ret;
+        generateParenthesisString(ret, test, n, n);
+        
         return ret;
     }
 
-    string generateParenthesisString(const string currentStr, int left, int right) {
+    int generateParenthesisString(vector<string> &data, string currentStr, int left, int right) {
         if (left <= 0 && right <= 0) {
             cout << currentStr << endl;
-            return string(currentStr);
+            data.push_back(currentStr);
+            return 0;
         }
 
-        if (left == right ) {
-            return generateParenthesisString(currentStr + "(", left - 1, right);
-        } else if (left < right) {
-            /* code */
-            for (int i = right ; i > left; i--) {
-                generateParenthesisString(currentStr + ")", left, i);
-            }
-            for (int i = left; i >= 0; i--) {
-                generateParenthesisString(currentStr + "(", i, right);
-            }
-        } else {
-            for (int i = left; i >= 0; i--) {
-                generateParenthesisString(currentStr + "(", i, right);
-            }
+        if (right > left && right > 0) {
+            generateParenthesisString(data, currentStr + ")", left, right - 1);
         }
-        return string(currentStr);
+        if (left > 0) {
+            generateParenthesisString(data, currentStr + "(", left - 1, right);
+        }
+        return 0;
     }
 };
 
